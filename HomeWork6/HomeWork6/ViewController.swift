e666af4edf9a072e73e0dd95f4d00e4153eb3861
likalizer <lika.lika1823@gmail.com>
2024-03-18 20:38:49 +0200

    Додані всі Action

--- HomeWork6/HomeWork6/ViewController.swift
+++ HomeWork6/HomeWork6/ViewController.swift
@@ -8,10 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func lipstickColour6(_ sender: Any) {
+    }
+    @IBAction func lipstickColour5(_ sender: Any) {
+    }
+    @IBAction func lipstickColour4(_ sender: Any) {
+    }
+    @IBAction func lipstickColour3(_ sender: Any) {
+    }
+    @IBAction func lipstickColour2(_ sender: Any) {
+    }
+    
+    @IBAction func lipstickColour1(_ sender: Any) {
+    }
+    
+    @IBAction func searchButton(_ sender: Any) {
+    }
+    
+    @IBAction func cartButton(_ sender: Any) {
+    }
+    
+    @IBAction func favoriteButton(_ sender: Any) {
+    }
+    
+    @IBAction func myNotino(_ sender: Any) {
+    }
+    @IBAction func exitToMainMenu(_ sender: Any) {
+    }
+    
+    @IBAction func shareButton(_ sender: Any) {
+    }
+    
+    @IBAction func addToCart(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var productVAT: UILabel!
+    @IBOutlet weak var productRate: UIImageView!
+    @IBOutlet weak var lipstickName: UILabel!
+    @IBOutlet weak var availabilityProduct: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+   
+        
+        
     }
 
 
